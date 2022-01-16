66417c1107de3d99c3e4b38161decbb6db57f837
cbenyah <69940664+cbenyah@users.noreply.github.com>
2022-01-16 01:30:00 +0000

    Navbar issues
    trying to make it display properly

--- nsbe_website/src/Components/Nav/Nav.js
+++ nsbe_website/src/Components/Nav/Nav.js
@@ -6,19 +6,21 @@ function Nav() {
 
     return (
         <Navbar bg="light" expand="lg">
+            <p> hello </p>
+
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                {/*<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
+    <Navbar.Collapse id="basic-navbar-nav">*/}
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#link">Link</Nav.Link>
                         
                     </Nav>
-                </Navbar.Collapse>
+                {/*</Navbar.Collapse>*/}
             </Container>
         </Navbar>
     );
 }
 
-export default Navbar;
+export default Nav;
