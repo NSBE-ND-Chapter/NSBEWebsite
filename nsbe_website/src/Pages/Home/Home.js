64acb75455d9939c05d7103d945e402b3bb92a63
Jonathan Lamptey <jlamptey@nd.edu>
2022-01-15 20:16:34 -0500

    Attempted to implement react navbar
    Attempted to implement react Navbar, experiencing react hook errors.

---
+++ nsbe_website/src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import Nav from "../../Components/Nav/Nav";
+
+
+function Home(){
+
+    return(
+        <div className="Header">
+            <Nav/>
+        </div>
+    );
+
+}
+
+export default Home;
