b2a4b04ec0b1f129cf7a9d298a192b355b4b6029
cbenyah <69940664+cbenyah@users.noreply.github.com>
2022-01-17 22:36:27 +0000

    Contact Us & Home Page

--- nbsewebsite/src/Components/Navigation/Navigation.js
+++ nbsewebsite/src/Components/Navigation/Navigation.js
@@ -10,29 +10,29 @@ import { Route, Routes, Link } from 'react-router-dom';
 export default class Navigation extends Component {
     render() {
         return (
-                <div>
-            <Navbar bg="dark" expand="lg" variant='dark'>
-                <Container>
-                    <Navbar.Brand href="/home">NBSE</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link as={Link} to='/home'>Home</Nav.Link>
-                            <Nav.Link as={Link} to='/contact'>Contact Us</Nav.Link>
-
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
             <div>
-                <Routes>
-                    <Route path = "/home" element={<Home/>}></Route>
-                    <Route path = "/contact" element={<ContactUs/>}></Route>
-                    <Route path = "/" element={<Home/>}></Route>
-                </Routes>
+                <Navbar bg="dark" expand="lg" variant='dark'>
+                    <Container>
+                        <Navbar.Brand href="/home">NBSE</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link as={Link} to='/home'>Home</Nav.Link>
+                                <Nav.Link as={Link} to='/contact'>Contact Us</Nav.Link>
+
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+                <div>
+                    <Routes>
+                        <Route path="/home" element={<Home />}></Route>
+                        <Route path="/contact" element={<ContactUs />}></Route>
+                        <Route path="/" element={<Home />}></Route>
+                    </Routes>
+                </div>
             </div>
-            </div>
-            
+
         );
     }
 }
--- nbsewebsite/src/Pages/ContactUs/ContactUs.js
+++ nbsewebsite/src/Pages/ContactUs/ContactUs.js
@@ -1,12 +1,111 @@
 
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 export default class ContactUs extends Component {
     render() {
         return (
             <div>
-                <h1>Contact Us</h1>
-                <p>This is contact US.</p>
+                <Container>
+                    <Row>
+                        <Col sm={2}>1 of 3</Col>
+                        <Col>
+                            <h1>Contact Us</h1>
+                            <p>This is contact US.</p>
+                            <br>
+                            </br>
+                            <Form>
+                                <Form.Group as={Row} className="mb-3" controlId="formHorizontalName">
+                                    <Form.Label column sm={2}>
+                                        Name
+                                    </Form.Label>
+                                    <Col sm={10}>
+                                        <Form.Control type="Name" placeholder="Name" />
+                                    </Col>
+                                </Form.Group>
+
+                                <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+                                    <Form.Label column sm={2}>
+                                        Email
+                                    </Form.Label>
+                                    <Col sm={10}>
+                                        <Form.Control type="Email" placeholder="Email" />
+                                    </Col>
+                                </Form.Group>
+                                <fieldset>
+                                    <Form.Group as={Row} className="mb-3">
+                                        <Form.Label as="legend" column sm={2}>
+                                            Concern
+                                        </Form.Label>
+                                        <Col sm={8}>
+                                            <Form.Check
+                                                type="radio"
+                                                label="Convention"
+                                                name="formHorizontalRadios"
+                                                id="formHorizontalRadios1"
+                                            />
+                                            <Form.Check
+                                                type="radio"
+                                                label="NSBE Club Activities"
+                                                name="formHorizontalRadios"
+                                                id="formHorizontalRadios2"
+                                            />
+                                            <Form.Check
+                                                type="radio"
+                                                label="Joining NSBE Board"
+                                                name="formHorizontalRadios"
+                                                id="formHorizontalRadios3"
+                                            />
+                                            <Form.Check
+                                                type="radio"
+                                                label="Other"
+                                                name="formHorizontalRadios"
+                                                id="formHorizontalRadios4"
+                                            />
+
+                                        </Col>
+                                    </Form.Group>
+                                </fieldset>
+
+                                <Form.Group as={Row} className="mb-2" controlId="exampleForm.ControlTextarea1">
+
+                                    <Form.Label column sm={2} >Please Explain Concern</Form.Label>
+
+                                    <Col sm={8}>
+                                        <Form.Control as="textarea" rows={3} />
+                                    </Col>
+                                </Form.Group>
+
+
+
+                                <Form.Group as={Row} className="mb-3" controlId="formHorizontalCheck">
+                                    <Col sm={{ span: 10, offset: 2 }}>
+                                    <Form.Check
+                                        required
+                                        label="Agree to terms and conditions"
+                                        feedback="You must agree before submitting."
+                                        feedbackType="invalid"
+                                    />
+                                    </Col>
+                                </Form.Group>
+
+                                <Form.Group as={Row} className="mb-3">
+                                    <Col sm={{ span: 10, offset: 2 }}>
+                                        <Button type="submit">Sign in</Button>
+                                    </Col>
+                                </Form.Group>
+                            </Form>
+                        </Col>
+                        <Col sm={2}> 3 of 3</Col>
+                    </Row>
+                </Container>
+
+
+
             </div>
         )
     }
