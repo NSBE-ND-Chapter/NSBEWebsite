a3b98faa2138c3252409f06a9ea78eacf13632c4
Jonathan Lamptey <jlamptey@nd.edu>
2022-01-21 16:27:39 -0500

    Added Footer
    Added Footer with Copyright and Creators details

--- nbsewebsite/src/App.js
+++ nbsewebsite/src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className='App'> 
       <Navigation/>
+      <Footer/>
     </div>
   );
 }
---
+++ nbsewebsite/src/Components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import { React,Component } from "react";
+
+import './Footer.css';
+
+export default class Footer extends Component{
+    render(){
+        return(
+            <div id="footer">
+                <p>&copy;2022 National Society for Black Engineers University of Notre Dame Chapter</p>
+                <p>Created by Caitlin Benyah & Jonathan Lamptey (Class of 2022)</p>
+            </div>
+        );
+    }
+}
