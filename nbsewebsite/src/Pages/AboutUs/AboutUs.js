e0d2d8bf814071676bf4a08ba87e9a8de31c6240
Jonathan Lamptey <jlamptey@nd.edu>
2022-02-10 14:32:38 -0500

    about us

--- nbsewebsite/src/Pages/AboutUs/AboutUs.js
+++ nbsewebsite/src/Pages/AboutUs/AboutUs.js
@@ -4,7 +4,7 @@ import './AboutUs.css';
 export default class AboutUs extends Component {
     render(){
         return(
-            <div>
+            <div className="AboutUs">
                 <h2>This is the About Us Page</h2>
                 <p>Over here we will have Mission, objectives and stuff like that</p>
             </div>
