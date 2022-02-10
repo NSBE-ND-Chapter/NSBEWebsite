60ee7b70239d1c95dd12af13d532c4564f642149
cbenyah <69940664+cbenyah@users.noreply.github.com>
2022-02-10 14:49:39 -0500

    update in AboutUS page

--- nbsewebsite/src/Pages/AboutUs/AboutUs.js
+++ nbsewebsite/src/Pages/AboutUs/AboutUs.js
@@ -2,11 +2,33 @@ import { React, Component } from "react";
 import './AboutUs.css';
 
 export default class AboutUs extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <div className="AboutUs">
-                <h2>This is the About Us Page</h2>
-                <p>Over here we will have Mission, objectives and stuff like that</p>
+                <p>
+                    <h3> Our Vision:  </h3>
+
+                    “We envision a world in which
+                    engineering is a mainstream word in homes
+                    and communities of color, and all Black
+                    students can envision themselves as
+                    engineers. In this world, Blacks exceed parity
+                    in entering engineering fields, earning
+                    degrees, and succeeding professionally.”
+
+                    <h3> Our Mission: </h3>
+
+                    "to increase the number of
+                    culturally responsible Black Engineers
+                    who excel academically, succeed
+                    professionally and positively impact the community."
+
+
+                    <h3> What does NSBE offer you? </h3>
+                    NSBE offers leadership training, professional development activities,
+                    mentoring, career placement services, community service opportunities
+                    and more.
+                </p>
             </div>
         );
     }
