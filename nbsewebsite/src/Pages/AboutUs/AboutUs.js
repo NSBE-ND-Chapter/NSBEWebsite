3693cc0e1e14a204f208806c84b01e6a348e1beb
Jonathan Lamptey <jlamptey@nd.edu>
2022-02-10 21:24:59 -0500

    Done
    Finished About Us page

--- nbsewebsite/src/Pages/AboutUs/AboutUs.js
+++ nbsewebsite/src/Pages/AboutUs/AboutUs.js
@@ -1,21 +1,23 @@
 import { React, Component } from "react";
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
 import './AboutUs.css';
 
 export default class AboutUs extends Component {
     render() {
         return (
             <div className="AboutUs">
-                {/* For Vision */}
-                <Row id="vision">
-                    <Col id="image">
-                        <img
-                            src={require('../../Media/Images/AboutUs/Sample.jpg')}
-                            alt="Vision"
-                        />
-                    </Col>
-                    <Col id="text">
-                    <h4> Our Vision:  </h4>
+                <Container>
+                    {/* For Vision */}
+                    {/*  <Row id="vision" xs={1} sm={1} md={2} lg={2}> */}
+                    <Row id="vision">
+                        <Col id="image">
+                            <img
+                                src={require('../../Media/Images/AboutUs/Sample.jpg')}
+                                alt="Vision"
+                            />
+                        </Col>
+                        <Col id="text">
+                            <h4> Our Vision:  </h4>
                             <p>
                                 “We envision a world in which
                                 engineering is a mainstream word in homes
@@ -25,19 +27,19 @@ export default class AboutUs extends Component {
                                 in entering engineering fields, earning
                                 degrees, and succeeding professionally.”
                             </p>
-                    </Col>
+                        </Col>
                     </Row>
 
                     {/* For Mission */}
-                <Row id="mission">
-                    <Col id="image">
-                        <img
-                            src={require('../../Media/Images/AboutUs/Sample.jpg')}
-                            alt="Mission"
-                        />
-                    </Col>
-                    <Col id="text">
-                    <h4> Our Mission: </h4>
+                    <Row id="mission">
+                        <Col id="image">
+                            <img
+                                src={require('../../Media/Images/AboutUs/Sample.jpg')}
+                                alt="Mission"
+                            />
+                        </Col>
+                        <Col id="text">
+                            <h4> Our Mission: </h4>
 
                             <p>
                                 "to increase the number of
@@ -45,28 +47,28 @@ export default class AboutUs extends Component {
                                 who excel academically, succeed
                                 professionally and positively impact the community."
                             </p>
-                    </Col>
+                        </Col>
                     </Row>
 
-                     {/* For what NSBE Offers */}
-                <Row id="offer">
-                    <Col id="image">
-                        <img
-                            src={require('../../Media/Images/AboutUs/Sample.jpg')}
-                            alt="Offer"
-                        />
-                    </Col>
-                    <Col id="text">
-                    <h4>What does NSBE offer you?</h4>
+                    {/* For what NSBE Offers */}
+                    <Row id="offer">
+                        <Col id="image">
+                            <img
+                                src={require('../../Media/Images/AboutUs/Sample.jpg')}
+                                alt="Offer"
+                            />
+                        </Col>
+                        <Col id="text">
+                            <h4>What does NSBE offer you?</h4>
 
                             <p>
                                 NSBE offers leadership training, professional development activities,
                                 mentoring, career placement services, community service opportunities
                                 and more.
                             </p>
-                    </Col>
+                        </Col>
                     </Row>
-                    
+                </Container>
             </div>
         );
     }
